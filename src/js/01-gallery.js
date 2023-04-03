6b90636eb2ca0833dbc8f52ef09f0da9bacfffb0
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-04-03 14:58:08 +0300

    дз8 задача1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,33 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+function createGallery(images) {
+  return images
+    .map(item => {
+      const { preview, original, description } = item;
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href=${original}>
+        <img class="gallery__image"
+        src=${preview}
+        alt='${description}'
+        />
+        </a>
+        </li>`;
+    })
+    .join('');
+}
+gallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
