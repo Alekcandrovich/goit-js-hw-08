c83560ea86c878e3ffb5bd6f0880422bf14e3e24
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-04-04 16:15:40 +0300

    дз8 задача3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    };
+
+    console.log(formData);
+
+    localStorage.removeItem('feedback-form-state');
+
+    form.reset();
+
+    form.elements.email.focus();
+});
+
+form.addEventListener('input', _.throttle(e => {
+    const formData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+}, 500));
+
+const saveFeedbackFormState = localStorage.getItem('feedback-form-state');
+
+if (saveFeedbackFormState) {
+    const { email, message } = JSON.parse(saveFeedbackFormState);
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+};
