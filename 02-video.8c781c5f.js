3fccb8543004fa627ad2fb342959c7a2e0d85fa4
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-04-05 10:45:18 +0000

    Deploying to gh-pages from @ Alekcandrovich/goit-js-hw-08@d30a22e308bb78cd99b7e4e0ab014d103b968ca3 🚀

--- 02-video.8c781c5f.js
+++
@@ -1,2 +0,0 @@
-const e=window.Vimeo,t=window.throttle,o=new e.Player("vimeo-player");o.on("timeupdate",t((e=>{localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));const r=localStorage.getItem("videoplayer-current-time");r&&o.setCurrentTime(r);
-//# sourceMappingURL=02-video.8c781c5f.js.map
